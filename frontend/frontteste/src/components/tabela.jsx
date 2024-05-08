4ee493d40ceccee58742d7ccd53619f4cf5a11f6
Marcelo <marccelo.moro@gmail.com>
2024-05-08 17:55:10 -0300

    inserindo tabela

---
+++ frontend/frontteste/src/components/tabela.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+
+function Tabela(){
+
+
+
+    return(
+
+        <div>
+             <table className='is-flex'>
+                <thead>
+                    <tr>
+                        <th className='is-size-5 pl-5 is-4'> Nome </th>
+                        <th className='is-size-5 pl-5 is-4'> Email  </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        
+                    </tr>
+                </tbody>
+           
+               
+            </table>
+
+        </div>
+       
+       
+    );
+}
+export default Tabela;
